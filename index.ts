b1bab6374435158315bfb71671d301a3dff02c9d
ca0v <coreyalix@hotmail.com>
2019-11-28 08:12:25 -0500

    reads from google photos api

--- index.ts
+++ index.ts
@@ -1,3 +1,107 @@
+declare var gapi: {
+    auth2: {
+        getAuthInstance: () => {
+            isSignedIn: {
+                listen: (cb: (isSignedIn: boolean) => void) => void;
+                get: () => boolean;
+            }
+            signIn: () => void;
+            signOut: () => void;
+        }
+    }
+    load: (type: string, cb: Function) => void;
+    client: {
+        init: (args: {
+            apiKey: string;
+            discoveryDocs: Array<string>;
+            clientId: string;
+            scope: string;
+        }) => Promise<any>;
+        photoslibrary: {
+            albums: {
+                list: Function;
+            }
+        }
+    }
+};
+
+class GooglePhotoSignin {
+    private peopleApiDiscovery = "";
+    // where to store these values?
+    private scopes = 'https://www.googleapis.com/auth/photoslibrary.readonly';
+    private authorizeButton = document.getElementById('authorize-button') as HTMLButtonElement;
+    private signoutButton = document.getElementById('signout-button') as HTMLButtonElement;
+    private ready = () => { };
+
+    async handleClientLoad() {
+        // Load the API client and auth2 library.
+        await new Promise((resolve, reject) => {
+            gapi.load('client:auth2', resolve);
+        });
+
+        let credentialsResponse = await fetch("./web/credentials.json");
+        let credentials: { apiKey: string, clientId: string } = await credentialsResponse.json();
+        let resp = await fetch('./web/photos_rest_v1.json');
+        this.peopleApiDiscovery = await resp.json();
+
+        await this.initClient(credentials);
+    }
+
+    private async initClient(args: { apiKey: string; clientId: string }) {
+        return new Promise<any>(async (good, bad) => {
+            this.ready = () => good();
+
+            await gapi.client.init({
+                apiKey: args.apiKey,
+                discoveryDocs: [this.peopleApiDiscovery],
+                clientId: args.clientId,
+                scope: this.scopes
+            });
+
+            // Listen for sign-in state changes.
+            gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
+            // Handle the initial sign-in state.
+            this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+            this.authorizeButton.onclick = this.handleAuthClick;
+            this.signoutButton.onclick = this.handleSignoutClick;
+        });
+    }
+
+    private updateSigninStatus(isSignedIn: boolean) {
+        if (isSignedIn) {
+            this.authorizeButton.style.display = 'none';
+            this.signoutButton.style.display = 'block';
+            this.ready();
+        } else {
+            this.authorizeButton.style.display = 'block';
+            this.signoutButton.style.display = 'none';
+        }
+    }
+
+    private handleAuthClick() {
+        gapi.auth2.getAuthInstance().signIn();
+    }
+
+    private handleSignoutClick() {
+        gapi.auth2.getAuthInstance().signOut();
+    }
+
+}
+
+class GooglePhotos {
+    async getAlbums() {
+        let signin = new GooglePhotoSignin();
+        await signin.handleClientLoad();
+        let resp = await gapi.client.photoslibrary.albums.list();
+        if (resp.status !== 200) throw `status: ${resp.status}`;
+        return (resp.result.albums) as Array<{
+            coverPhotoBaseUrl: string;
+        }>;
+    }
+}
+
+let photos = new GooglePhotos();
+
 function getPanels() {
     let panels = Array.from(document.querySelectorAll(`.panel[data-id]`)) as HTMLElement[];
     console.log(panels.map(p => p.dataset["id"]).join(","));
@@ -67,10 +171,6 @@ function merge_nodes(node1: HTMLElement, node2: HTMLElement) {
     reindex();
 }
 
-function rotate_node(node: HTMLElement, deg: string) {
-    node.style.transform = `rotate(${deg}deg)`;
-}
-
 // create a canvas of the entire collage
 async function asCanvas() {
     return new Promise<HTMLCanvasElement>((good, bad) => {
@@ -109,7 +209,6 @@ class Repl {
         let [verb, noun, noun2] = command.split(" ");
         switch (verb) {
             case "export":
-                debugger;
                 let canvas = await asCanvas();
                 if (!canvas) return;
                 let img = document.createElement("img");
@@ -195,7 +294,7 @@ class Repl {
 
 let repl = new Repl();
 
-function startup() {
+async function startup() {
     let cmd = document.querySelector(".console") as HTMLInputElement;
     cmd.onkeydown = (event) => {
         console.log(event);
@@ -205,6 +304,16 @@ function startup() {
         }
     }
     reindex();
+
+    const target = document.querySelector(".photos");
+    if (target) {
+        const albums = await photos.getAlbums();
+        albums.forEach(album => {
+            const img = document.createElement("img");
+            img.src = album.coverPhotoBaseUrl;
+            target.appendChild(img);
+        });
+    }
 }
 
 startup();
