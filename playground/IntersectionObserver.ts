7c5b451f70b00753b2ed0078d2cda80655b64df5
Corey Alix <coreyalix@hotmail.com>
2019-12-23 10:19:51 -0500

    adds IntersectionObserver playground for hit detection

---
+++ playground/IntersectionObserver.ts
@@ -0,0 +1,48 @@
+function randomWalk(node) {
+  let x = Math.random() * 200;
+  let y = Math.random() * 200;
+  node.style.left = x + "px";
+  node.style.top = y + "px";
+}
+
+async function CollisionDetection() {
+  let canNotMove = Array.from(document.querySelectorAll(".can-collide.can-not-move")) as Array<HTMLElement>;
+  let canMove = Array.from(document.querySelectorAll(".can-collide.can-move")) as Array<HTMLElement>;
+
+  let root = canNotMove[0];
+  let target = canMove[0];
+
+  return new Promise((good, bad) => {
+    let observer = new IntersectionObserver(
+      (entries: IntersectionObserverEntry[]) => {
+        console.log(entries);
+        let entry = entries.find(entry => entry.isIntersecting);
+        document.title = !entry ? "Outside" : Math.round(entry.intersectionRatio * 100) + "%";
+        if (!!entry && 1.0 === entry.intersectionRatio) {
+          good();
+        }
+      },
+      {
+        root: root,
+        rootMargin: "0px",
+        threshold: 0.1,
+      }
+    );
+    observer.observe(target);
+  });
+}
+
+function StartRandomWalk() {
+  let canMove = Array.from(document.querySelectorAll(".can-collide.can-move"));
+  canMove.forEach(n => randomWalk(n));
+  return canMove.map(mover => {
+    return setInterval(() => randomWalk(mover), 1000);
+  });
+}
+
+let walkers = StartRandomWalk();
+
+CollisionDetection().then(() => {
+  walkers.forEach(w => clearInterval(w));
+  console.log("Hit!");
+});
