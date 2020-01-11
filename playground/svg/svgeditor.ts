7f73b23e178f8b6ea13677a026b632a086bc9538
ca0v <coreyalix@hotmail.com>
2020-01-11 00:16:49 -0500

    allows primitive editing

--- playground/svg/svgeditor.ts
+++ playground/svg/svgeditor.ts
@@ -12,9 +12,14 @@ class SvgEditor {
     private gridOverlay: SVGSVGElement;
     private workPath: SVGPathElement;
     private cursorPath: SVGPathElement;
+    private sourcePath: SVGPathElement;
     private gridScale = 10;
+    private currentIndex = -1;
 
-    constructor(public workview: SVGSVGElement) {
+    constructor(public workview: SVGSVGElement, public input: HTMLElement) {
+        this.sourcePath = this.workview.querySelector("path") as SVGPathElement;
+        if (!this.sourcePath) throw "workview must have a path";
+
         let { x, y, width, height } = workview.viewBox.baseVal;
         this.gridOverlay = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
         this.gridOverlay.setAttribute("viewBox", `${x} ${y} ${width} ${height}`);
@@ -30,15 +35,117 @@ class SvgEditor {
             "stroke-width": "0.2",
         });
         this.gridOverlay.appendChild(this.cursorPath);
+
+        let keystate = <any>{};
+
+        input.addEventListener("keyup", event => {
+            keystate[event.code] = false;
+        });
+
+        input.addEventListener("keydown", event => {
+            keystate[event.code] = true;
+            switch (event.code) {
+                case "ArrowUp":
+                    if (keystate["ControlLeft"]) {
+                        this.setPath(this.sourcePath, this.transform({ dx: 0, dy: -1 }).join(""));
+                    } else {
+                        this.focus(document.activeElement?.previousElementSibling);
+                    }
+                    break;
+                case "ArrowDown":
+                    if (keystate["ControlLeft"]) {
+                        this.setPath(this.sourcePath, this.transform({ dx: 0, dy: 1 }).join(""));
+                    } else {
+                        this.focus(document.activeElement?.nextElementSibling);
+                    }
+                    break;
+                case "ArrowLeft":
+                    if (keystate["ControlLeft"]) {
+                        let path = this.transform({ dx: -1, dy: 0 }).join("\n");
+                        this.setPath(this.sourcePath, path);
+                        break;
+                    }
+                case "ArrowRight":
+                    if (keystate["ControlLeft"]) {
+                        let path = this.transform({ dx: 1, dy: 0 });
+                        this.setPath(this.sourcePath, path.join("\n"));
+                        break;
+                    }
+                default:
+                    console.log(event.code);
+            }
+        });
+
+    }
+
+    private transform(translate: { dx: number, dy: number }) {
+        let index = this.currentIndex;
+        let path = this.getPath().split("\n");
+        if (!path) throw "use targetPath";
+        let [c, ...a] = path[index].split(" ");
+        let command = { command: c, args: a.map(parseFloat) };
+        switch (command.command) {
+            case "A": {
+                let [rx, ry, a, b, cw, x, y] = command.args;
+                x += translate.dx;
+                y += translate.dy;
+                path[index] = this.stringify({ command: command.command, args: [rx, ry, a, b, cw, x, y] });
+                this.setPath(this.cursorPath, this.drawCursor({ x, y }));
+                break;
+            }
+            case "C": {
+                let [ax, ay, bx, by, x, y] = command.args;
+                x += translate.dx;
+                y += translate.dy;
+                path[index] = this.stringify({ command: command.command, args: [ax, ay, bx, by, x, y] });
+                this.setPath(this.cursorPath, this.drawCursor({ x, y }));
+                break;
+            }
+            case "M":
+            case "T":
+            case "L": {
+                let [x, y] = command.args;
+                x += translate.dx;
+                y += translate.dy;
+                path[index] = this.stringify({ command: command.command, args: [x, y] });
+                this.setPath(this.cursorPath, this.drawCursor({ x, y }));
+                break;
+            }
+        }
+        this.showMarkers();
+        (this.input.children[index] as HTMLDivElement).innerText = path[index];
+        return path;
+    }
+
+    stringify(command: Command) {
+        return `${command.command} ${command.args.join(" ")}`;
+    }
+
+    focus(element: any) {
+        if (!element) return;
+        if (!element.focus) return;
+        element.focus();
     }
 
     goto(index: number) {
+        this.currentIndex = index;
         let path = this.getPath().split("\n");
         if (!path) return;
-        let [command, ...args] = path[index].split(" ");
-        switch (command) {
-            case "L": {
-                let [x, y] = args.map(parseFloat);
+        let [c, ...a] = path[index].split(" ");
+        let command = { command: c, args: a.map(parseFloat) };
+
+        switch (command.command) {
+            case "A": {
+                let [rx, ry, a, b, cw, x, y] = command.args;
+                this.setPath(this.cursorPath, this.drawCursor({ x, y }));
+                break;
+            }
+            case "C":
+            case "L":
+            case "M":
+            case "S":
+            case "T": {
+                let [x, y] = command.args;
                 this.setPath(this.cursorPath, this.drawCursor({ x, y }));
                 break;
             }
@@ -62,9 +169,7 @@ class SvgEditor {
         return path;
     }
 
-    getPath() {
-        let path = this.workview.querySelector("path");
-        if (!path) return "";
+    getPath(path = this.sourcePath) {
         let d = getComputedStyle(path).getPropertyValue("d");
         let commands = this.parsePath(d);
         return commands.map(c => `${c.command} ${c.args.join(" ")}`).join("\n");
@@ -91,9 +196,7 @@ class SvgEditor {
     }
 
     showMarkers() {
-        let path = this.workview.querySelector("path");
-        if (!path) return;
-        let d = getComputedStyle(path).getPropertyValue("d");
+        let d = getComputedStyle(this.sourcePath).getPropertyValue("d");
         let commands = this.parsePath(d);
         let overlayPath = this.createOverlayPoint(commands);
         overlayPath.unshift("M 0 0");
@@ -173,7 +276,7 @@ class SvgEditor {
         if (firstQuote < 0) throw "no quote found";
         let lastQuote = path.lastIndexOf("\"");
         if (lastQuote <= firstQuote) throw "no end quote found";
-        path = path.substring(firstQuote + 1, lastQuote - 1);
+        path = path.substring(firstQuote + 1, lastQuote);
         let tokens = path.split("");
         let commands = [] as Array<{ command: string, args: number[] }>;
         let commandArgs = [];
@@ -186,7 +289,7 @@ class SvgEditor {
                     .join("")
                     .split(" ")
                     .map(v => v.trim())
-                    .filter(v => !!v)
+                    .filter(v => v !== "")
                     .map(v => parseFloat(v));
                 commands.push({ command: ch, args });
                 commandArgs = [];
