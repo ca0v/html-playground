43afd1ec9536833e4511c1eb48fbd80dd0a09fe6
ca0v <coreyalix@hotmail.com>
2020-08-05 22:19:06 -0400

    fixes signing workflow

--- collage/controls/GooglePhotoSignin.ts
+++ collage/controls/GooglePhotoSignin.ts
@@ -6,9 +6,12 @@ export class GooglePhotoSignin {
   private peopleApiDiscovery = "";
   // where to store these values?
   private scopes = "https://www.googleapis.com/auth/photoslibrary.readonly";
-  private authorizeButton = document.getElementById("authorize-button") as HTMLButtonElement;
-  private signoutButton = document.getElementById("signout-button") as HTMLButtonElement;
+
+  private authorizeButton: HTMLButtonElement|null = null;
+  private signoutButton: HTMLButtonElement|null = null;
+
   private ready = () => { };
+
   async handleClientLoad() {
     // Load the API client and auth2 library.
     await new Promise((resolve, reject) => {
@@ -23,10 +26,18 @@ export class GooglePhotoSignin {
     this.peopleApiDiscovery = await resp.json();
     await this.initClient(credentials);
   }
+
   private async initClient(args: {
     apiKey: string;
     clientId: string;
   }) {
+
+    this.authorizeButton = document.getElementById("authorize-button") as HTMLButtonElement;
+    if (!this.authorizeButton) throw "a clickable element must exist with id of 'authorize-button'";
+
+    this.signoutButton = document.getElementById("signout-button") as HTMLButtonElement;
+    if (!this.signoutButton) throw "a clickable element must exist with id of 'signout-button'";
+
     return new Promise<any>(async (good, bad) => {
       this.ready = () => good();
       await gapi.client.init({
@@ -36,27 +47,30 @@ export class GooglePhotoSignin {
         scope: this.scopes,
       });
       // Listen for sign-in state changes.
-      gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
+      gapi.auth2.getAuthInstance().isSignedIn.listen(status => this.updateSigninStatus(status));
       // Handle the initial sign-in state.
       this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      this.authorizeButton.onclick = this.handleAuthClick;
-      this.signoutButton.onclick = this.handleSignoutClick;
+      this.authorizeButton!.onclick = () => this.handleAuthClick();
+      this.signoutButton!.onclick = () => this.handleSignoutClick();
     });
   }
+
   private updateSigninStatus(isSignedIn: boolean) {
     if (isSignedIn) {
-      this.authorizeButton.style.display = "none";
-      this.signoutButton.style.display = "block";
+      this.authorizeButton!.style.display = "none";
+      this.signoutButton!.style.display = "block";
       this.ready();
     }
     else {
-      this.authorizeButton.style.display = "block";
-      this.signoutButton.style.display = "none";
+      this.authorizeButton!.style.display = "block";
+      this.signoutButton!.style.display = "none";
     }
   }
+
   private handleAuthClick() {
     gapi.auth2.getAuthInstance().signIn();
   }
+
   private handleSignoutClick() {
     gapi.auth2.getAuthInstance().signOut();
   }
